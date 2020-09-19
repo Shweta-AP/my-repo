5dd54741a5e5d64993bb93393616ecf63ed641ba
Shweta-AP <39293999+Shweta-AP@users.noreply.github.com>
2020-09-19 10:52:31 +0530

    Add files via upload
    file added

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.shweta.jpa.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
